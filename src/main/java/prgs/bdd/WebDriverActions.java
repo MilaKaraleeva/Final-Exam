5c8a67294c1779b37041c92ffa604e183891cbd0
karal <mila.karaleeva@mgu.bg>
2025-01-11 22:07:53 +0200

    Final Exam for Automation QA Added acceptCookies method

--- src/main/java/prgs/bdd/WebDriverActions.java
+++ src/main/java/prgs/bdd/WebDriverActions.java
@@ -3,6 +3,8 @@ package prgs.bdd;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.Random;
@@ -12,9 +14,25 @@ public class WebDriverActions {
     private final WebDriver webDriver;
     private final int wait;
 
+
+
     public WebDriverActions(WebDriver webDriver, int wait) {
         this.webDriver = webDriver;
         this.wait = wait;
+    }
+
+    public void acceptCookies(String webUrl) {
+        WebDriver.Timeouts timeouts;
+        timeouts = webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(wait));
+        webDriver.get(webUrl);
+        try {
+            WebElement acceptCookiesButton = webDriver.findElement(By.className("fc-button fc-cta-consent fc-primary-button")); // Example with 'id'
+            if (acceptCookiesButton.isDisplayed()) {
+                acceptCookiesButton.click();
+            }
+        } catch (Exception e) {
+            System.out.println("No cookies prompt found.");
+        }
     }
 
     //url load + find element + click
@@ -22,42 +40,20 @@ public class WebDriverActions {
         WebDriver.Timeouts timeouts;
         timeouts = webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(wait));
         webDriver.get(webUrl);
-        WebElement enterButton=webDriver.findElement(By.xpath(htmlButton));
+        WebElement enterButton=webDriver.findElement(By.id(htmlButton));
         enterButton.click();
 
     }
 
-    public String emailGenerator(){
-        WebDriver.Timeouts timeouts;
-        timeouts = webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(wait));
-        Random rnd = new Random();
-        String newEmail= "test.test"+rnd+"@gmail.com";
-        while (newEmail.length() <= 20) {
-            return newEmail;
-        }
-        return newEmail;
-    }
-    public String passwordGenerator(){
-        WebDriver.Timeouts timeouts;
-        timeouts = webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(wait));
-        Random rnd = new Random();
-        String newPassword= "ABC"+rnd+"&%$";
-        while (newPassword.length() <= 20) {
-            return newPassword;
-        }
-        return newPassword;
-    }
-
-
     //url load + find element + type in
-    public String enterTextInField(String webUrl,String htmlField, String textToWrite){
+    public String enterTextInField(String webUrl,String idField, String textToWrite){
         WebDriver.Timeouts timeouts;
         timeouts = webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(wait));
         webDriver.get(webUrl);
-        WebElement inputField = webDriver.findElement(By.xpath(htmlField));
+        WebElement inputField = webDriver.findElement(By.id(idField));
         inputField.clear();
         inputField.sendKeys(textToWrite);
-        return inputField.getAttribute("value");
+        return inputField.getDomAttribute("value");
     }
 
     //url load + find element + get text
@@ -69,5 +65,25 @@ public class WebDriverActions {
         return messageText.getText();
     }
 
+    public String emailGenerator(){
+        WebDriver.Timeouts timeouts;
+        timeouts = webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(wait));
+        Random rnd = new Random();
+        String newEmail= "test.test"+rnd+"@gmail.com";
+        while (newEmail.length() <= 20) {
+            return newEmail;
+        }
+        return newEmail.toString();
+    }
+    public String passwordGenerator(){
+        WebDriver.Timeouts timeouts;
+        timeouts = webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(wait));
+        Random rnd = new Random();
+        String newPassword= "ABC"+rnd+"&%$";
+        while (newPassword.length() <= 20) {
+            return newPassword;
+        }
+        return newPassword.toString();
+    }
 
 }
--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -1,5 +1,5 @@
 package stepdefinitions;
-/*This is where your test code, including Cucumber step definitions, will reside.*/
+/*test code, including Cucumber step definitions, will reside.*/
 
 import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.And;
@@ -7,15 +7,22 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.AfterAll;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import prgs.bdd.WebDriverActions;
+import java.net.CookieHandler;
+import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class LoginSteps {
 
     static WebDriver webDriver;
-    static int wait=5;
+    static int wait=10;
     static String webUrl = "https://practice.automationtesting.in/my-account/";
     static String element;
     static String myAccount="<a href=\"https://practice.automationtesting.in/my-account/\">My Account</a>";
@@ -44,33 +51,59 @@ public class LoginSteps {
     //Login for Test User Details feature Background steps Login and Navigate under "My Account"
     @Given("The user is successfully logged in to the web page")
     public void the_user_is_successfully_logged_in_to_the_web_page(){
-        WebDriverActions webDriverActions = getWebDriverActions();
-        element="//*[@id=\"n2-ss-6\"]/div[1]/div/div/div[1]/div[1]/img";
-        String findElement=webDriverActions.findElementDoExist(webUrl,element);
+
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("fc-button fc-cta-consent fc-primary-button")));
+        WebDriverActions webDriverActions=new WebDriverActions(webDriver,10);
+        webDriver.get(webUrl);
+        webDriverActions.acceptCookies(webUrl);
+        String emailField="//*[@id=\"reg_email\"]";
+        webDriverActions.enterTextInField(webUrl,emailField,webDriverActions.emailGenerator());
+        String passwordField="//*[@id=\"reg_password\"]";
+        webDriverActions.enterTextInField(webUrl,passwordField,webDriverActions.passwordGenerator());
+        String registerButton="";
+        webDriverActions.clickButton(webUrl,registerButton);
+        String loadImage="";
+        String findElement=webDriverActions.findElementDoExist(webUrl,loadImage);
         assert findElement.contains("Shop Selenium Books");
     }
 
-    private static WebDriverActions getWebDriverActions() {
-        WebDriverActions webDriverActions=new WebDriverActions(webDriver,wait);
+
+   /* private static WebDriverActions getEmail() {
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("reg_password")));
+        WebDriverActions webDriverActions=new WebDriverActions(webDriver,10);
+        webDriver.get(webUrl);
         String emailField="//*[@id=\"reg_email\"]";
-        String passwordField="//*[@id=\"reg_password\"]";
         webDriverActions.enterTextInField(webUrl,emailField,webDriverActions.emailGenerator());
-        webDriverActions.enterTextInField(webUrl,passwordField,webDriverActions.passwordGenerator());
-        String registerButton="//*[@id=\"customer_login\"]/div[2]/form/p[3]/input[3]";
-        webDriverActions.clickButton(webUrl,registerButton);
         return webDriverActions;
     }
+    private static WebDriverActions getPassword() {
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("reg_password")));
+        String passwordField="//*[@id=\"reg_password\"]";
+        webDriverActions.enterTextInField(webUrl,passwordField,webDriverActions.passwordGenerator());
+        return webDriverActions;
+    }*/
+
+
+
+
 
     @When("The user clicks on the {string} link")
     public void the_user_clicks_on_the_My_Account_link(){
         WebDriverActions webDriverActions=new WebDriverActions(webDriver,wait);
         webDriverActions.clickButton(webUrl,myAccount);
+        String findElement=webDriverActions.findElementDoExist(webUrl,element);
+        assert findElement.contains("");
 
     }
     @And("The user navigates to {string}")
     public void the_user_navigates_to_Account_Details(){
         WebDriverActions webDriverActions=new WebDriverActions(webDriver,wait);
         webDriverActions.clickButton(webUrl,accountDetails);
+        String findElement=webDriverActions.findElementDoExist(webUrl,element);
+        assert findElement.contains("");
     }
 
     @Then("The email address displayed under the string textbox matches the expected email")
@@ -78,6 +111,7 @@ public class LoginSteps {
         element="";
         WebDriverActions webDriverActions=new WebDriverActions(webDriver,wait);
         webDriverActions.findElementDoExist(webUrl,element);
+
 
     }
 
